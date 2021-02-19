393e5f3fadd327a16985177e2dd45f5550f4818a
bing-deng <804433988@qq.com>
2021-02-19 10:55:00 +0900

    引入 Parse 到RN ,测试成功

--- App.js
+++ App.js
@@ -25,9 +25,41 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-community/async-storage';
+
+// ES6 Minimized
+const Parse = require('parse/react-native.js');
+Parse.initialize("4CqI88sgCyhVozqeNldH7bfrNx3mUxXxf2SRnHjZ", "ln3sCP4oWq8E1TvWPHVRvBbM5VMlUbF1XkxIxZvK");
+Parse.serverURL = 'https://parseapi.back4app.com/'
+//javascriptKey is required only if you have it on server.
+
+
+
+Parse.setAsyncStorage(AsyncStorage);
+
+
+
+const GameScore = Parse.Object.extend("GameScore");
+const gameScore = new GameScore();
+
+gameScore.set("score", 123321);
+gameScore.set("playerName", "deng Plott");
+gameScore.set("cheatMode", false);
+
+gameScore.save()
+.then((gameScore) => {
+  // Execute any logic that should take place after the object is saved.
+  alert('New object created with objectId: ' + gameScore.id);
+}, (error) => {
+  // Execute any logic that should take place if the save fails.
+  // error is a Parse.Error with an error code and message.
+  alert('22Failed to create new object, with error code: ' + error.message);
+});
 
 const App: () => React$Node = () => {
   return (
+
+
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
